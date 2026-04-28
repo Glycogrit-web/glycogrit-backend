91626ac5a4209527e587845b61bdc635ced0e855
Yashwant Gahlot <gyash2558@gmail.com>
2026-04-28 18:44:26 +0530

    fix: Correct down_revision reference in tier payment migration

--- alembic/versions/20260428_fix_tier_payment_consistency.py
+++ alembic/versions/20260428_fix_tier_payment_consistency.py
@@ -1,7 +1,7 @@
 """Fix tier requires_payment consistency
 
 Revision ID: fix_tier_payment
-Revises: 20260428_add_event_activity_types
+Revises: 20260428_activity_types
 Create Date: 2026-04-28
 
 """
@@ -11,7 +11,7 @@ import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
 revision = 'fix_tier_payment'
-down_revision = '20260428_add_event_activity_types'
+down_revision = '20260428_activity_types'
 branch_labels = None
 depends_on = None
 
